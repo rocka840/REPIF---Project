32bfa75fd187df064f9874b40a29593368a6346d
Katherine <47138336+rocka840@users.noreply.github.com>
2022-03-18 08:51:11 +0100

    Add files via upload

--- Login.php
+++ Login.php
@@ -8,17 +8,45 @@
 
 <body>    
 
-    <h1>Log-In into Database of User - REPIF</h1>
+    <?php
+        if(isset($_POST["LastName"], $_POST["FirstName"],$_POST["Psw"])){
+           $sqlLog = $connection->prepare("Select * from User where LastName, FirstName, Email, Psw = ????");
+           $sqlLog->bind_param("ssss", $_POST["LastName"], $_POST["FirstName"],$_POST["Psw"]);
+           $sqlLog->execute();
 
-    <form method="POST" action="UsersPage.php">  
-        <div class="container">   
-            <label>Username : </label>   
-            <input type="text" placeholder="Enter Username" name="username" required>  
-            <label>Password : </label>   
-            <input type="password" placeholder="Enter Password" name="password" required>  
-            <button type="submit">Login</button> 
-        </div>   
-    </form>
+           if($selectionWentOK){
+               $result = $sqlLog->get_result();
+               while ($row = $result->fetch_assoc()){
+                ?>
+                <h1>Log-In into Database of User - REPIF</h1>
+
+                <form method="POST" action="UsersPage.php">  
+                    <div class="container">   
+                        <label>Last Name:</label>   
+                        <input type="text" name="LastName" required>
+                        <label>First Name:</label>   
+                        <input type="text" name="FirstName" required>
+                        <label>Technician?</label>
+                        <input type="checkbox" name="Tech">
+                        <br>
+                        <label>Email:</label>   
+                        <input type="text" name="Email" required>
+                        <label>Password:</label>   
+                        <input type="password" placeholder="Enter Password" name="Psw" required>
+                        <button type="submit">Login</button> 
+                    </div>   
+                </form>
+                <?php
+                
+               }
+           }
+        } else {
+            print "Something went wrong when selecting data";
+        }
+    ?>
+
+
+    
 
 </body>
 
